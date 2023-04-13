7a321af285479406fcd25fd77608cd5e4f705e12
meita33 <124659672+meita33@users.noreply.github.com>
2023-04-13 15:06:29 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,5 +9,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述
       # username == 'admin' && password == '2222'
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+keys: [:employee_number, :first_name_kanji, :last_name_kanji, :first_name_kana, :last_name_kana, :password, :email])
   end
 end
